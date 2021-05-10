1d6bec4dea8711a4b3c8a1bcdd502afda4c2a0ed
atlaua18 <nastyaopen@yandex.ru>
2021-05-10 14:52:19 +0300

    add site and component

---
+++ 01-first-project/my-app/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="https://cryptologos.cc/logos/binance-coin-bnb-logo.png" alt=""></img>
+      </header>
+    );
+}
+
+export default Header;
---
+++ 01-first-project/my-app/src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://cdn.dribbble.com/users/1869172/screenshots/15627559/media/b77ddf94aabcff17cfb391e9dd8676aa.png?compress=1&resize=1000x750"
+                    alt=""
+                ></img>
+            </div>
+            <div>
+                ava + description
+                <img
+                    src="https://cdn.dribbble.com/users/2131993/screenshots/15628402/media/7bb0d27e44d8c2eff47276ae86bfd6a3.png?compress=1&resize=1000x750"
+                    alt=""
+                ></img>
+            </div>
+            <div>
+                My posts
+                <div>New posts</div>
+            </div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+    );
+}
