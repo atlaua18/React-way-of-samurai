82b4f4a9dffd35c4f42905ba007924c960180ce7
atlaua18 <nastyaopen@yandex.ru>
2021-05-13 16:53:12 +0300

    add avatars in dialogs

--- 01-first-project/my-app/src/redux/state.tsx
+++ 01-first-project/my-app/src/redux/state.tsx
@@ -7,14 +7,15 @@ let state = {
     },
     MessagePage: {
         dialogsData: [
-            { id: 1, name: "Nastya" },
-            { id: 2, name: "Vitaliq" },
-            { id: 3, name: "Mother" },
+            { id: 1, name: "Nastya", avatar: "https://cdn.dribbble.com/users/4051260/screenshots/15643099/media/610a2b7dc0d934af453f846d349292be.jpeg?compress=1&resize=1000x750" },
+            { id: 2, name: "Vitaliq", avatar: "https://cdn.dribbble.com/users/984375/screenshots/15647057/media/0c127c5f15c5778d36f5ace71760f579.jpg?compress=1&resize=1000x750" },
+            { id: 3, name: "Mother", avatar: "https://cdn.dribbble.com/users/1162650/screenshots/15637865/media/df7d2c7a8570d2db9d07ea46f68057d5.jpg?compress=1&resize=800x600" },
         ],
         messagesData: [
             { id: 1, message: "Hi" },
             { id: 2, message: "Yo" },
             { id: 3, message: "How are you?" },
+            { id: 4, message: "I'm fine" },
         ],
     },
 };
