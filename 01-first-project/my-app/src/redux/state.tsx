9bdd0738431a325153479503bd4996d0c40727a7
atlaua18 <nastyaopen@yandex.ru>
2021-05-20 00:12:31 +0300

    add store in state.tsx

--- 01-first-project/my-app/src/redux/state.tsx
+++ 01-first-project/my-app/src/redux/state.tsx
@@ -3,77 +3,143 @@ import { IMessagesData } from "../interfaces/IMessagesData";
 import { IPost } from "../interfaces/IPost";
 // import { rerenderEntireTree } from "../render";
 
-let rerenderEntireTree = (s: IAppState) => {
-    console.log("state change")
-};
-
-let state: IAppState = {
-    profilePage: {
-        postsData: [
-            { id: 1, message: "Hi, how are you?", likesCount: 15 },
-            { id: 2, message: "It's my first post", likesCount: 20 },
-        ],
-        newPostText: "",
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                { id: 1, message: "Hi, how are you?", likesCount: 15 },
+                { id: 2, message: "It's my first post", likesCount: 20 },
+            ],
+            newPostText: "",
+        },
+        messagePage: {
+            dialogsData: [
+                {
+                    id: 1,
+                    name: "Nastya",
+                    avatar: "https://cdn.dribbble.com/users/4051260/screenshots/15643099/media/610a2b7dc0d934af453f846d349292be.jpeg?compress=1&resize=1000x750",
+                },
+                {
+                    id: 2,
+                    name: "Vitaliq",
+                    avatar: "https://cdn.dribbble.com/users/984375/screenshots/15647057/media/0c127c5f15c5778d36f5ace71760f579.jpg?compress=1&resize=1000x750",
+                },
+                {
+                    id: 3,
+                    name: "Mother",
+                    avatar: "https://cdn.dribbble.com/users/1162650/screenshots/15637865/media/df7d2c7a8570d2db9d07ea46f68057d5.jpg?compress=1&resize=800x600",
+                },
+            ],
+            messagesData: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "Yo" },
+                { id: 3, message: "How are you?" },
+                { id: 4, message: "I'm fine" },
+            ],
+        },
     },
-    messagePage: {
-        dialogsData: [
-            {
-                id: 1,
-                name: "Nastya",
-                avatar: "https://cdn.dribbble.com/users/4051260/screenshots/15643099/media/610a2b7dc0d934af453f846d349292be.jpeg?compress=1&resize=1000x750",
-            },
-            {
-                id: 2,
-                name: "Vitaliq",
-                avatar: "https://cdn.dribbble.com/users/984375/screenshots/15647057/media/0c127c5f15c5778d36f5ace71760f579.jpg?compress=1&resize=1000x750",
-            },
-            {
-                id: 3,
-                name: "Mother",
-                avatar: "https://cdn.dribbble.com/users/1162650/screenshots/15637865/media/df7d2c7a8570d2db9d07ea46f68057d5.jpg?compress=1&resize=800x600",
-            },
-        ],
-        messagesData: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "Yo" },
-            { id: 3, message: "How are you?" },
-            { id: 4, message: "I'm fine" },
-        ],
+    _callSubscriber (s: IAppState) {
+        console.log("state change")
+    },
+
+    getState() {
+        return this._state;
+    },    
+    subscribe (observer: any) {
+        this._callSubscriber = observer; //паттерн Наблюдатель(observer)
+    },
+    
+    addPost() {
+        let newPost: IPost = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0,
+        };
+        this._state.profilePage.postsData.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText (newText: string) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    sendMessage (messageText: string){
+        let newMessage: IMessagesData = {
+            id: 5,
+            message: messageText,
+        };
+        this._state.messagePage.messagesData.push(newMessage);
+    
+        this._callSubscriber(this._state);
     },
 };
+export default store;
 
-interface FinInterface {
-    () : void;
-}
+// let rerenderEntireTree = (s: IAppState) => {
+//     console.log("state change")
+// };
 
-export const addPost: FinInterface = () => {
-    let newPost: IPost = {
-        id: 3,
-        message: state.profilePage.newPostText,
-        likesCount: 0,
-    };
-    state.profilePage.postsData.push(newPost);
-    state.profilePage.newPostText = "";
-    rerenderEntireTree(state);
-};
+// let state: IAppState = {
+//     profilePage: {
+//         postsData: [
+//             { id: 1, message: "Hi, how are you?", likesCount: 15 },
+//             { id: 2, message: "It's my first post", likesCount: 20 },
+//         ],
+//         newPostText: "",
+//     },
+//     messagePage: {
+//         dialogsData: [
+//             {
+//                 id: 1,
+//                 name: "Nastya",
+//                 avatar: "https://cdn.dribbble.com/users/4051260/screenshots/15643099/media/610a2b7dc0d934af453f846d349292be.jpeg?compress=1&resize=1000x750",
+//             },
+//             {
+//                 id: 2,
+//                 name: "Vitaliq",
+//                 avatar: "https://cdn.dribbble.com/users/984375/screenshots/15647057/media/0c127c5f15c5778d36f5ace71760f579.jpg?compress=1&resize=1000x750",
+//             },
+//             {
+//                 id: 3,
+//                 name: "Mother",
+//                 avatar: "https://cdn.dribbble.com/users/1162650/screenshots/15637865/media/df7d2c7a8570d2db9d07ea46f68057d5.jpg?compress=1&resize=800x600",
+//             },
+//         ],
+//         messagesData: [
+//             { id: 1, message: "Hi" },
+//             { id: 2, message: "Yo" },
+//             { id: 3, message: "How are you?" },
+//             { id: 4, message: "I'm fine" },
+//         ],
+//     },
+// };
 
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-};
+// export const addPost = () => {
+//     let newPost: IPost = {
+//         id: 3,
+//         message: state.profilePage.newPostText,
+//         likesCount: 0,
+//     };
+//     state.profilePage.postsData.push(newPost);
+//     state.profilePage.newPostText = "";
+//     rerenderEntireTree(state);
+// };
 
-export const sendMessage = (messageText: string) => {
-    let newMessage: IMessagesData = {
-        id: 5,
-        message: messageText,
-    };
-    state.messagePage.messagesData.push(newMessage);
+// export const updateNewPostText = (newText: string) => {
+//     state.profilePage.newPostText = newText;
+//     rerenderEntireTree(state);
+// };
 
-    rerenderEntireTree(state);
-};
+// export const sendMessage = (messageText: string) => {
+//     let newMessage: IMessagesData = {
+//         id: 5,
+//         message: messageText,
+//     };
+//     state.messagePage.messagesData.push(newMessage);
 
-export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer; //паттерн Наблюдатель(observer)
-}
+//     rerenderEntireTree(state);
+// };
 
-export default state;
+// export const subscribe = (observer: any) => {
+//     rerenderEntireTree = observer; //паттерн Наблюдатель(observer)
+// };
